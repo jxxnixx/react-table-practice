29582f5181d19d37b139f7fc87b7cfe0f87a4d0e
yeonjuKimxx <dkepffk10@sookmyung.ac.kr>
2024-08-19 15:36:50 +0900

    feat: React Table Init Setting

---
+++ src/app/nav.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className="absolute bg-white h-[80px] w-screen text-black flex flex-row gap-40 justify-center items-center">
+      <Link href={'/columnOrder'}>ColumnOrderTable</Link>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/component/type.ts
@@ -0,0 +1,11 @@
+export type PushAlert = {
+  id: number;
+  title: string;
+  frequency: string;
+  status: string;
+  startDate: string;
+  endDate: string;
+  OS: string;
+  sent: number;
+  openRatio: number;
+};
